0e0a7aff394943564b09276347ffa2e6c58422a5
Md. Jakaria <jakaria.dcc11@gmail.com>
2015-12-01 02:28:02 +0600

    Pool Table
    Only view table

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class Controller {
+    @FXML
+    protected GridPane gridPane ;
+    @FXML
+    protected Pane pane ;
+    @FXML
+    protected ImageView imageView ;
+
+}
